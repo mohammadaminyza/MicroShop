2518e097f2a88adb2c9b744bd1515bb5ccb1104e
mohammadaminyza <mohamad.aminyaghoobzadeh@gmail.com>
2022-03-10 14:27:13 +0330

    Add Linq Utilites Refrence Project Install Mongodb

---
+++ src/Common/MicroShop.Common/Utilities/Linq.cs
@@ -0,0 +1,9 @@
+﻿namespace MicroShop.Common.Utilities;
+
+public static class Linq
+{
+    public static bool IsNullOrEmpty(this string value)
+    {
+        return string.IsNullOrEmpty(value);
+    }
+}
--- src/Services/Catalogs/01. Core/MicroShop.Catalogs.Core.Domain/Common/ValueObjects/Name.cs
+++ src/Services/Catalogs/01. Core/MicroShop.Catalogs.Core.Domain/Common/ValueObjects/Name.cs
@@ -1,4 +1,6 @@
-﻿using MicroShop.Common.ValueObjects;
+﻿using MicroShop.Common.Exceptions;
+using MicroShop.Common.Utilities;
+using MicroShop.Common.ValueObjects;
 
 namespace MicroShop.Catalogs.Core.Domain.Common.ValueObjects;
 
@@ -9,8 +11,19 @@ public class Name : BaseValueObject<Name>
     public override bool ObjectIsEqual(Name otherObject) => Value == otherObject.Value;
     public override int ObjectGetHashCode() => Value.GetHashCode();
 
+    public static explicit operator string(Name name) => name.Value;
+    public static implicit operator Name(string name) => new(name);
+
+    private Name()
+    {
+        Value = string.Empty;
+    }
+
     public Name(string value)
     {
+        if (value.IsNullOrEmpty())
+            throw new InvalidValueObjectStateException("Value Can't Be NullOrEmpty");
+
         Value = value;
     }
 }
