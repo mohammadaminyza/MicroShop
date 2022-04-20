406fea064d30f3cf71276a7379fc0d2cba2bde33
mohammadaminyza <mohamad.aminyaghoobzadeh@gmail.com>
2022-04-20 15:30:36 +0430

    Add Mongo UnitOfWork

--- src/Common/MicroShop.Common/Data/Repository/MongodbBaseCommandRepository.cs
+++ src/Common/MicroShop.Common/Data/Repository/MongodbBaseCommandRepository.cs
@@ -1,83 +1,24 @@
-﻿using MicroShop.Common.ValueObjects;
-using System.Linq.Expressions;
-using MicroShop.Common.Data.Context;
-using MicroShop.Common.Data.Context.Mongodb;
+﻿using MicroShop.Common.Data.Context.Mongodb;
+using MicroShop.Common.Data.UnitOfWork;
 using MicroShop.Common.Entities;
+using MicroShop.Common.ValueObjects;
+using System.Linq.Expressions;
+using JetBrains.Annotations;
 
 namespace MicroShop.Common.Data.Repository;
 
-public class MongodbBaseCommandRepository<TEntity, TDbContext> : ICommandRepository<TEntity> where TEntity : AggregateRoot where TDbContext : MongodbContext
+public class MongodbBaseCommandRepository<TEntity, TDbContext> : MongodbUnitOfWork<TDbContext>, ICommandRepository<TEntity> where TEntity : AggregateRoot where TDbContext : MongodbContext
 {
     private readonly TDbContext _dbContext;
 
-    public MongodbBaseCommandRepository(TDbContext dbContext)
+    public MongodbBaseCommandRepository(TDbContext dbContext) : base(dbContext)
     {
         _dbContext = dbContext;
     }
 
-    public void BeginTransaction()
-    {
-        throw new NotSupportedException();
-    }
-
-    public int Commit()
-    {
-        throw new NotSupportedException();
-    }
-
-    public Task<int> CommitAsync()
-    {
-        throw new NotSupportedException();
-    }
-
-    public void CommitTransaction()
-    {
-        throw new NotSupportedException();
-    }
-
-    public void Delete(Id id)
-    {
-        throw new NotSupportedException();
-    }
-
-    public void Delete(TEntity entity)
-    {
-        throw new NotSupportedException();
-    }
-
-    public void DeleteGraph(long id)
-    {
-        throw new NotSupportedException();
-    }
-
-    public void DeleteRange(IEnumerable<TEntity> entities)
-    {
-        throw new NotSupportedException();
-    }
-
-    public bool Exists(Expression<Func<TEntity, bool>> expression)
-    {
-        throw new NotSupportedException();
-    }
-
-    public Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
-    {
-        throw new NotSupportedException();
-    }
-
-    public TEntity GetGraph(Id id)
-    {
-        throw new NotSupportedException();
-    }
-
-    public Task<TEntity> GetGraphAsync(Id businessId)
-    {
-        throw new NotSupportedException();
-    }
-
     public void Insert(TEntity entity)
     {
-        throw new NotSupportedException();
+        throw new NotImplementedException();
     }
 
     public async Task InsertAsync(TEntity entity)
@@ -87,16 +28,51 @@ public class MongodbBaseCommandRepository<TEntity, TDbContext> : ICommandReposit
 
     public void InsertRange(IEnumerable<TEntity> entities)
     {
-        throw new NotSupportedException();
+        throw new NotImplementedException();
     }
 
-    public Task InsertRangeAsync(IEnumerable<TEntity> entities)
+    public async Task InsertRangeAsync(IEnumerable<TEntity> entities)
     {
-        throw new NotSupportedException();
+        throw new NotImplementedException();
     }
 
-    public void RollbackTransaction()
+    public void Delete(Id id)
     {
-        throw new NotSupportedException();
+        throw new NotImplementedException();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteGraph(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteRange(IEnumerable<TEntity> entities)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TEntity GetGraph(Id id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TEntity> GetGraphAsync(Id businessId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Exists(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ src/Common/MicroShop.Common/Data/UnitOfWork/MongodbUnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using MicroShop.Common.Data.Context.Mongodb;
+
+namespace MicroShop.Common.Data.UnitOfWork;
+
+public class MongodbUnitOfWork<TContext> : IUnitOfWork where TContext : MongodbContext
+{
+    private readonly TContext _dbContext;
+
+    public MongodbUnitOfWork(TContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void BeginTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void CommitTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RollbackTransaction()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Commit()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<int> CommitAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
